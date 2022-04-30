1c63254ab98c503382aa2780b6dc9bf8d41ca096
Daguer <dgrfps@gmail.com>
2022-04-30 19:54:45 -0300

    Update core.cpp

--- src/core.cpp
+++ src/core.cpp
@@ -7,15 +7,7 @@
 #define INCASE(v) if((v))
 #define EXIT(vd) { vd; return 1; }
 
-uint8_t buffer[42]; //THE OUTPUT BYTE BUFFER
-
-/*  
-    PROGRAM OUTPUT:
-
-    uint8_t buffer[42] {
-        67,111,110,118,101,114,116,105,110,103,32,115,116,114,105,110,103,32,116,111,32,98,121,116,101,115,32,97,110,100,32,118,105,99,101,45,118,101,114,115,97,33
-    };
-*/
+uint8_t buffer[42]; //THE OUTPUT BYTE BUFFER, YOU MAY NOW NEED THIS AND THE LINE 29/30.
 
 int main(int argc, char **argv)
 {
